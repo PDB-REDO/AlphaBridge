bd41392564ed2ada8c59f1197745525822dd2fa0
Daniel_laptop <Daalsa98@gmail.com>
2025-09-22 17:27:32 +0200

    change in run_alphabridge_screen.sh: uncommented "set -euo pipefail" and changed default value of SKIP_EXISTING to false.

--- run_alphabridge_screen.sh
+++ run_alphabridge_screen.sh
@@ -46,7 +46,7 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --skip-existing)
-            SKIP_EXISTING=true
+            SKIP_EXISTING=false
             shift
             ;;
         --plot)
